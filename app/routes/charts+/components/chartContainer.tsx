5f4a7b67d039e4f9b515c3ddce48c40b06bd4018
Evan Marie Carr <evanmariecarr@gmail.com>
2024-05-09 22:59:02 -0400

    charts

--- app/routes/charts+/components/chartContainer.tsx
+++ app/routes/charts+/components/chartContainer.tsx
@@ -1,3 +1,4 @@
+import CenterFull from "~/components/buildingBlocks/centerFull";
 import Flex from "~/components/buildingBlocks/flex";
 import FlexFull from "~/components/buildingBlocks/flexFull";
 
@@ -7,8 +8,10 @@ export default function ChartContainer({
   children: React.ReactNode;
 }) {
   return (
-    <Flex className="w-95% max-w-95% max-h-95% p-[1vh] bg-slate-900 bg-gradient-to-br from-violet-900/50 via-indigo-900/50 to-cyan-900/50">
-      {children}
-    </Flex>
+    <CenterFull>
+      <Flex className="w-95% max-w-95% max-h-95% p-[1vh] bg-slate-900 bg-gradient-to-br from-violet-900/50 via-indigo-900/50 to-cyan-900/50 border-900-md shadowBroadLooser">
+        {children}
+      </Flex>
+    </CenterFull>
   );
 }
